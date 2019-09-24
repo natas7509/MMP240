80e4c663a43d6dd31e700c8b5e468b3d0217f784
jon darling <jondarling@jons-MacBook-Pro.local>
2019-09-24 16:13:26 -0400

    added brixton mmp460

---
+++ mmp460brixtonMeadows/js/functions.js
@@ -0,0 +1,27 @@
+$(window).scroll(function() {
+  var wScroll = $(this).scrollTop();
+
+  // Landing Elements
+  if (wScroll > $(".vendor-pics").offset().top - $(window).height() / 0.9) {
+    $(".vendor-pics figure").each(function(i) {
+      setTimeout(function() {
+        $(".vendor-pics figure")
+          .eq(i)
+          .addClass("is-showing");
+      }, 300 * Math.exp(i * 0.14) - 300);
+    });
+  }
+
+  // Promoscope
+  if (wScroll > $(".large-window").offset().top - $(window).height()) {
+    $(".large-window").css({
+      "background-position":
+        "center " + (wScroll - $(".large-window").offset().top) + "px"
+    });
+
+    var opacity =
+      (wScroll - $(".large-window").offset().top + 400) / (wScroll / 5);
+
+    $(".window-tint").css({ opacity: opacity });
+  }
+});
