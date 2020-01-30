0ec8d1e21824d629438c33c802fd90823fb613ea
jon darling <jondarling@jons-MacBook-Pro.local>
2020-01-30 00:55:03 -0500

    js vendors

--- Midterm-MainSite/js/functions.js
+++ Midterm-MainSite/js/functions.js
@@ -1,34 +1,37 @@
-
-$(window).scroll(function(){
+$(window).scroll(function () {
 
     var wScroll = $(this).scrollTop();
-   
+
 
 
 
     // Landing Elements
-    if(wScroll > $('.vendor-pics').offset().top - ($(window).height() / 1.2)) {
+    if (wScroll > $('.vendor-pics').offset().top - ($(window).height() / 1.2)) {
 
-        $('.vendor-pics figure').each(function(i){
+        $('.vendor-pics figure').each(function (i) {
 
-            setTimeout(function(){
+            setTimeout(function () {
                 $('.vendor-pics figure').eq(i).addClass('is-showing');
-            }, (700 * (Math.exp(i * 0.14))) - 700);
+            }, (700 * (Math.exp(i * .14))) - 700);
         });
 
     }
 
 
     // Promoscope
-    if(wScroll > $('.large-window').offset().top - $(window).height()){
+    if (wScroll > $('.large-window').offset().top - $(window).height()) {
 
-        $('.large-window').css({'background-position':'center '+ (wScroll - $('.large-window').offset().top) +'px'});
+        $('.large-window').css({
+            'background-position': 'center ' + (wScroll - $('.large-window').offset().top) + 'px'
+        });
 
         var opacity = (wScroll - $('.large-window').offset().top + 400) / (wScroll / 5);
 
-        $('.window-tint').css({'opacity': opacity});
+        $('.window-tint').css({
+            'opacity': opacity
+        });
 
     }
-   
+
 
 });
